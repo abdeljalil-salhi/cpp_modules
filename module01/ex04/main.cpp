47c1c7acd16d879e56dea461fe53d55c53a6ae05
Abdel <abdelisloading@gmail.com>
2023-03-04 19:58:52 +0100

    CPP 01 ex 04: Sed is for losers

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: absalhi <absalhi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 02:09:53 by absalhi           #+#    #+#             */
-/*   Updated: 2023/03/04 02:49:29 by absalhi          ###   ########.fr       */
+/*   Updated: 2023/03/04 19:57:45 by absalhi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(int argc, char **argv)
 			exit(EXIT_FAILURE);
 
 	std::string filename = argv[1], s1 = argv[2], s2 = argv[3], _line;
-	size_t _find;
+	size_t _pos, _findPos;
 
 	std::ifstream _ifile(filename);
 	if (!_ifile.is_open())
@@ -36,10 +36,32 @@ int main(int argc, char **argv)
 
 	while (std::getline(_ifile, _line))
 	{
-		_find = 0;
-		while ((_find = _line.find(s1, _find)) != std::string::npos)
-			_line.replace(_find, s1.length(), s2), _find += s2.length();
-		_ofile << _line << "\n";
+		std::string _updatedLine;
+		_pos = 0UL;
+		while (_pos < _line.length())
+		{
+			/*
+			** find() returns the index of the first occurrence of the substring
+			** in the string from given starting position.
+			**
+			** Find the position of the next occurrence of `s1` in the line
+			** starting from position `_pos`.
+			** If `s1` is not found in the whole line, std::string::npos is returned.
+			*/
+			if ((_findPos = _line.find(s1, _pos)) == std::string::npos)
+			{
+				_updatedLine += _line.substr(_pos);
+				break;
+			}
+			/*
+			** If `s1` is found, append the substring from `_pos` to `_findPos`
+			** to the updated line, then append `s2` to the updated line.
+			** Update `_pos` to the position after the occurrence of `s1`.
+			*/
+			_updatedLine += _line.substr(_pos, _findPos - _pos) + s2;
+			_pos = _findPos + s1.length();
+		}
+		_ofile << _updatedLine << "\n";
 	}
 	_ifile.close(), _ofile.close();
 	std::cout << "File " << filename

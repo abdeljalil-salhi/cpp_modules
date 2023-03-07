678bbc72982a6e1425d9b4004e908b70e8ee7942
Abdel <abdelisloading@gmail.com>
2023-03-07 22:29:06 +0100

    CPP 02 ex 00: My First Class in Orthodox Canonical Form

--- module02/ex00/Fixed.cpp
+++ module02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: absalhi <absalhi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 01:30:09 by absalhi           #+#    #+#             */
-/*   Updated: 2023/03/06 04:33:38 by absalhi          ###   ########.fr       */
+/*   Updated: 2023/03/07 22:28:10 by absalhi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ Fixed::Fixed(void)
 	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(Fixed const &other)
-	: _value(other._value)
+Fixed::Fixed(Fixed const &rhs)
 {
 	std::cout << "Copy constructor called" << std::endl;
+	*this = rhs;
 }
 
 Fixed::~Fixed(void)
@@ -29,10 +29,10 @@ Fixed::~Fixed(void)
 	std::cout << "Destructor called" << std::endl;
 }
 
-Fixed &Fixed::operator=(Fixed const &other)
+Fixed &Fixed::operator=(Fixed const &rhs)
 {
 	std::cout << "Copy assignment operator called" << std::endl;
-	this->_value = other._value;
+	this->_value = rhs.getRawBits();
 	return *this;
 }
 
@@ -48,8 +48,8 @@ int Fixed::getRawBits(void) const
 	return this->_value;
 }
 
-std::ostream &operator<<(std::ostream &os, Fixed const &obj)
+std::ostream &operator<<(std::ostream &os, Fixed const &rhs)
 {
-	os << obj.getRawBits();
+	os << rhs.getRawBits();
 	return os;
 }
--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: absalhi <absalhi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 01:22:21 by absalhi           #+#    #+#             */
-/*   Updated: 2023/03/06 04:30:11 by absalhi          ###   ########.fr       */
+/*   Updated: 2023/03/07 22:27:18 by absalhi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ class Fixed
 	public:
 
 		Fixed( void );
-		Fixed( Fixed const &other );
+		Fixed( Fixed const &rhs );
 		~Fixed( void );
-		Fixed &operator=( Fixed const &other );
+		Fixed &operator=( Fixed const &rhs );
 
 		void setRawBits( int const raw );
 
@@ -40,6 +40,6 @@ class Fixed
 
 };
 
-std::ostream &operator<<(std::ostream &os, Fixed const &obj);
+std::ostream &operator<<(std::ostream &os, Fixed const &rhs);
 
 #endif

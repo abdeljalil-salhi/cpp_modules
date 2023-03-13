b5bc0b08775c5670db03abc1373d829825f3dadb
Abdel <abdelisloading@gmail.com>
2023-03-13 14:36:47 +0100

    CPP 06 ex 01: Serialization

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: absalhi <absalhi@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 20:17:33 by absalhi           #+#    #+#             */
-/*   Updated: 2023/03/12 20:24:59 by absalhi          ###   ########.fr       */
+/*   Updated: 2023/03/13 14:35:08 by absalhi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,19 @@ int main(void)
 {
 	{
 		std::cout << GREEN << "Basic tests: " << RESET << std::endl;
+		Data *_data = new Data();
+		Serializer _serializer;
+		uintptr_t _raw = _serializer.serialize(_data);
+		Data *_data2 = _serializer.deserialize(_raw);
+		std::cout << "data: " << _data << std::endl;
+		std::cout << "data2: " << _data2 << std::endl;
 	}
-	Data *_data = new Data();
-	Serializer _serializer;
-	uintptr_t _raw = _serializer.serialize(_data);
-	Data *_data2 = _serializer.deserialize(_raw);
-	std::cout << "data: " << _data << std::endl;
-	std::cout << "data2: " << _data2 << std::endl;
 	{
-		std::cout << GREEN << "Random tests: " << RESET << std::endl;
+		std::cout << GREEN << "Randomness: " << RESET << std::endl;
 		Data *_data = new Data();
 		Serializer _serializer;
 		uintptr_t _raw = _serializer.serialize(_serializer.deserialize(_serializer.serialize(_data)));
-		Data *_data2 = _serializer.deserialize(_raw);
+		Data *_data2 = _serializer.deserialize(_serializer.serialize(_serializer.deserialize(_raw)));
 		std::cout << "data: " << _data << std::endl;
 		std::cout << "data2: " << _data2 << std::endl;
 	}
